fb941a3bcfc0f0aaf866c64bcc2efe2f5121c3e3
ANaik611 <167113732+ANaik611@users.noreply.github.com>
2025-05-06 10:30:25 +0530

    delete that prev vr file and use this instead

---
+++ frontend/src/components/vr.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Vr = () => {
+  return (
+    <div className="flex justify-center items-center p-4">
+      <iframe
+	src="https://weshopai-weshopai-virtual-try-on.hf.space"
+	frameborder="0"
+	width="850"
+	height="450"
+></iframe>
+
+    </div>
+  );
+};
+
+export default Vr;
